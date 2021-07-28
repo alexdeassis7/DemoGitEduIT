818527a28a509e70fc63726069723a1c40d0c45c
alex <alex@gmail.com>
2021-07-28 12:32:59 -0300

    solucione un error critico

--- tutorial_maven/tutorial_maven_jar/src/main/java/org/educacionit/App.java
+++ tutorial_maven/tutorial_maven_jar/src/main/java/org/educacionit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World hoy nos toco resolver el tkt que me asigno mi lider tecnico !" );
+        System.out.println( "Hello World hoy nos toco resolver el tkt que me asigno mi lider tecnico bueno a mi tambien me todo modificar este archivo e incluso esta linea !" );
     }
 }
