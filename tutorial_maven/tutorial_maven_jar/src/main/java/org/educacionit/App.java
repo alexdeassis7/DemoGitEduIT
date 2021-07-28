424edfa3138e60a8e9e51100537a109d2f69ba13
alex <alex@gmail.com>
2021-07-28 12:15:24 -0300

    se resolvio el tkt 5693 modificando la clase App.java

--- tutorial_maven/tutorial_maven_jar/src/main/java/org/educacionit/App.java
+++ tutorial_maven/tutorial_maven_jar/src/main/java/org/educacionit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World hoy nos toco resolver el tkt que me asigno mi lider tecnico !" );
     }
 }
